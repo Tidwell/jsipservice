6bbef04dffba1e15ab2d21c05971d266ae290294
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-02-18 20:18:05 -0500

    homepage

--- index.php
+++ index.php
@@ -9,14 +9,17 @@ $allowed = array_merge($pages,$docs);
 $path = explode('/', $_SERVER['PATH_INFO']);
 $route = $path[1];
 
-if (in_array($route, $allowed)) {
-	$page = $path[1];
-	require('./templates/index.html');
-}
-
-else if ($route === 'REST') {
+if ($route === 'REST') {
 	require('./models/rest.php');
 	$rest = new RestService('GET');
 	$rest->handleRawRequest($_SERVER, $_GET, $_POST);
+}
+else if (in_array($route, $allowed)) {
+	$page = $path[1];
+	require('./templates/index.html');
+}
+else {
+	$page = 'home';
+	require('./templates/index.html');
 }
 ?>
