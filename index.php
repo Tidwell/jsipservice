65b991133756c4f5fcef46cba4d7363922bd4473
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-02-18 20:14:40 -0500

    fixing allowed

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 error_reporting(E_ALL);
 
 $pages = ['home','docs','contact'];
+$docs = ['bootstrap'];
+
+$allowed = array_merge($pages,$docs);
+
 $path = explode('/', $_SERVER['PATH_INFO']);
 $route = $path[1];
 
-if (in_array($route, $pages)) {
+if (in_array($route, $allowed)) {
 	$page = $path[1];
 	require('./templates/index.html');
 }
